e7982d2903ee57c0def6abaf9a5f7c46debe8de3
rstr5105@gmail.com <h1llz90210>
2014-08-25 00:23:56 -0700

    Fixed MarkovChain.genChains().  I wasn't generating the inner HashMap Properly.  Next thing to do is implement saving chains.  Very happy with this threaded Markov Chain Generator.  It's very simple, which is GOOD!

--- src/com/badwater/Markov/MarkovChain.java
+++ src/com/badwater/Markov/MarkovChain.java
@@ -20,19 +20,22 @@ public class MarkovChain implements Serializable {
 			//if chains contains the primary key, check to see if it contains nextKey
 			if ( chains.containsKey ( priKey ) ) {
 				if ( chains.get ( priKey ).containsKey ( nextKey ) ) {
-					//if it does, grab the integer Value of that key and add 1. Then remove that key from
+					//if it does, grab the integer Value of that key and add 1.  Then remove that key from
 					//chains[priKey] and re add it with the new word count.
 					int tmp = chains.get ( priKey ).get ( nextKey ) + 1;
-					chains.get ( priKey ).remove ( nextKey );
 					chains.get ( priKey ).put ( nextKey, tmp );
+				}
+				else {
+					chains.get ( priKey ).put ( nextKey, 1 );
 				}
 			}
 			//if chains does NOT contain the primary key
-			else if ( !chains.containsKey ( priKey ) ) {
+			else {
 				//create a temporary hashmap to store our values in.
 				HashMap<String, Integer> tmpMap = new HashMap<String, Integer> ();
+
 				tmpMap.put ( nextKey, 1 );
-				//put our tmpMap int chains[key]
+				//put our tmpMap into chains[key]
 				chains.put ( priKey, tmpMap );
 			}
 		}
